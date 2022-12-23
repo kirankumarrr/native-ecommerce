8a7cb38cfcfdf6a65964ccb5fa087b3116b9881d
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-12-23 14:44:15 +0530

    Infrastructure setup

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,41 +1,39 @@
 import React from "react";
-import styled from "styled-components";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-const Title = styled.Text`
-  padding: 16px;
-  color: red;
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-export default function RestaurantInfoCard({ restaurant = {} }) {
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled(Text)`
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "default Name",
-    icon = "default icon",
-    photos = ["https://picsum.photos/700"],
-    address = "default address",
-    isOpenNow = false,
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = "default isClosedTemporarily",
+    isClosedTemporarily,
   } = restaurant;
 
   return (
-    <View style={styles.list}>
-      <Card style={styles.card}>
-        <Card.Title title={name} />
-        <Card.Cover source={{ uri: photos[0] }} />
-        <Title>{address}</Title>
-      </Card>
-    </View>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  card: {
-    padding: 16,
-  },
-});
+};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,40 +1,31 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
-import RestaurantInfoCard from "../components/restaurant-info-card.component";
+import styled from "styled-components/native";
 
-export default function RestaurantsScreen() {
-  const [searchQuery, setSearchQuery] = React.useState("");
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-  const onChangeSearch = (query) => setSearchQuery(query);
-  return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <RestaurantInfoCard />
-      </SafeAreaView>
-    </>
-  );
-}
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-});
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
